f53dec4aa3d7768c46d5f424ac0e07ed072bf948
Shanshan Chen <shanshanc.chen@gmail.com>
2019-03-21 19:53:17 +0100

    passed

--- index.js
+++ index.js
@@ -1,7 +1,27 @@
 function StringChanges(str) { 
-
   // code goes here  
-  return str; 
+  let splitted = str.split('');
+  let newArr = [];
+  let counter = 0;
+  while (counter < splitted.length) {
+    if (splitted[counter] === 'M') {
+      if (splitted[counter-1] && splitted[counter-1] !== 'M') {
+        newArr.push(splitted[counter-1]);
+      }
+      counter++;
+    } else if (splitted[counter] === 'N') {
+      // console.log(counter);
+      counter = counter + 2;
+      // console.log(counter);
+    } else {
+      newArr.push(splitted[counter]);
+      counter++;
+    }
+  }
+  
+  let newStr = newArr.join('');
+
+  return newStr; 
          
 }
 
