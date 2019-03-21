28a81338a376d035f36fffc4ba4927b7146459d6
Felix Weinberger <felix.weinberger@gmail.com>
2019-03-21 19:50:33 +0100

    feat(dude): whoaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa

--- index.js
+++ index.js
@@ -1,8 +1,22 @@
 function StringChanges(str) { 
 
   // code goes here  
-  return str; 
+  const arr = str.split('');
+
+  const res = [];
+  for (let i = 0; i < arr.length; i++) {
+    if (arr[i] === 'M') {
+      res.push(arr[i - 1] !== 'M' ? arr[i - 1] : '' || '');
+    } else if (arr[i] === 'N') {
+      i += 1;
+    } else {
+      res.push(arr[i]);
+    }
+  }
+  return res.join(''); 
          
 }
+
+console.log(StringChanges('abcNdgM'))
 
 module.exports = StringChanges;
